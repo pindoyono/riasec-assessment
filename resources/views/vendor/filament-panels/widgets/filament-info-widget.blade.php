81f02e8579584bf396775bb3fc4ecc6d61c299e8
pindoyono <pindoyonopatan@gmail.com>
2026-04-29 08:51:35 +0800

    tulisan panduan

--- resources/views/vendor/filament-panels/widgets/filament-info-widget.blade.php
+++ resources/views/vendor/filament-panels/widgets/filament-info-widget.blade.php
@@ -1,10 +1,9 @@
 <div class="fi-wi-info-widget">
-    <div class="fi-wi-info-widget-header">
+    <div class="fi-wi-info-widget-header" style="display: flex; align-items: center; justify-content: space-between;">
         <span class="fi-wi-info-widget-title" style="font-size: 1.3rem; font-weight: bold;">ABM SMK Kaltara</span>
-    </div>
-    <div class="fi-wi-info-widget-links" style="margin-top: 1rem;">
         <a href="https://drive.google.com/drive/folders/1hRWGDKVT_dXgf_W6i46CeAkq9ag_BwHc?usp=sharing" target="_blank"
-            rel="noopener" style="color: #f59e42; font-weight: 600; text-decoration: underline;">Panduan</a>
+            rel="noopener"
+            style="color: #f59e42; font-weight: 600; text-decoration: underline; margin-left: 1rem;">Panduan</a>
     </div>
     <div class="fi-wi-info-widget-version" style="margin-top: 1rem; color: #888;">
         v2.0
